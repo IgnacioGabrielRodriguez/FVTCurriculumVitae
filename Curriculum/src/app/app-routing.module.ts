262df49ab62f0fe5364ca858b64f77a0380f82de
IgnacioGabrielRodriguez <igrodriguez@escuelasproa.edu.ar>
2023-11-21 14:00:05 -0300

    Ruteo

--- Curriculum/src/app/app-routing.module.ts
+++ Curriculum/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ModeladoComponent } from './pages/modelado/modelado.component';
+import { GraficoComponent } from './pages/grafico/grafico.component';
+import { ProgramacionComponent } from './pages/programacion/programacion.component';
+import { ExtrasComponent } from './pages/extras/extras.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:  '', redirectTo: 'home' , pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'modelado', component: ModeladoComponent},
+  {path: 'grafico', component: GraficoComponent},
+  {path: 'programacion', component: ProgramacionComponent},
+  {path: 'extras', component: ExtrasComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Curriculum/src/app/app.module.ts
+++ Curriculum/src/app/app.module.ts
@@ -2,15 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeadComponent } from './shared/head/head.component';
+import { NavComponent } from './shared/nav/nav.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    FooterComponent,
+    NavComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
